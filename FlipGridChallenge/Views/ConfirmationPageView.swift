f72b1ab4c3ee5dd8f271b311becd0753c29c2b32
Rob Evans <robevans@microsoft.com>
2022-01-31 20:18:45 -0500

    Update ConfirmationPageView.swift
    added icons for name, email, website

--- FlipGridChallenge/Views/ConfirmationPageView.swift
+++ FlipGridChallenge/Views/ConfirmationPageView.swift
@@ -14,12 +14,13 @@ struct ConfirmationPageView: View {
         VStack {
             HeaderView(text: "Hello, \(userProperty.firstName)!", subText: "Your super-awesome portfolio has been successfully submitted!  The details below will be public within your community!")
                 .padding()
-            Text(userProperty.website)
-            Text(userProperty.firstName)
-            Text(userProperty.emailAddress)
+            confirmationRowView(image: "globe", name: userProperty.website)
+            confirmationRowView(image: "person.fill", name: userProperty.firstName)
+            confirmationRowView(image: "envelope.fill", name: userProperty.emailAddress)
             Spacer()
             ButtonView(text: "Sign In", enabled: true)
-                .padding()
+                .padding(.horizontal)
+                .padding(.vertical, 5)
         }
         .background(Color("background"))
     }
@@ -30,5 +31,20 @@ struct ConfirmationPageView_Previews: PreviewProvider {
         ConfirmationPageView()
         ConfirmationPageView()
             .colorScheme(.dark)
+    }
+}
+
+struct confirmationRowView: View {
+    let image: String
+    let name: String
+
+    var body: some View {
+        HStack {
+            Image(systemName: image)
+                .font(.title2)
+            Text(name)
+                .font(.title2)
+        }
+        .padding()
     }
 }
