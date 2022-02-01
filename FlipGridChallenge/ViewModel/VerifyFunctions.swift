562f068461903efa14dfe75cebea35c2ca02e28a
Rob Evans <robevans@microsoft.com>
2022-01-31 19:52:43 -0500

    renamed view to viewModel

--- FlipGridChallenge/ViewModel/VerifyFunctions.swift
+++ FlipGridChallenge/ViewModel/VerifyFunctions.swift
@@ -21,11 +21,10 @@ class VerifyFunctions: ObservableObject {
     }
 
     func validEmail(_ email: String) -> Bool {
-        if email.contains("@") {
-            return true
-        } else {
-            return false
-        }
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
 
     }
 
